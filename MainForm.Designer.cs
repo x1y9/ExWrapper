7993e9e22600faccbc9f6ee11075412d04787268
x1y9 <x1y9@hotmail.com>
2024-10-25 23:40:02 +0800

    better layout

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -48,12 +48,17 @@
             this.radioCall = new System.Windows.Forms.RadioButton();
             this.radioEmbed = new System.Windows.Forms.RadioButton();
             this.btnSelectIcon = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.groupCall.SuspendLayout();
             this.groupEmbed.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupCall
             // 
+            this.groupCall.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.groupCall.Controls.Add(this.btnOpen);
             this.groupCall.Controls.Add(this.label3);
             this.groupCall.Controls.Add(this.textDir);
@@ -61,11 +66,9 @@
             this.groupCall.Controls.Add(this.textPara);
             this.groupCall.Controls.Add(this.label1);
             this.groupCall.Controls.Add(this.textRun);
-            this.groupCall.Location = new System.Drawing.Point(63, 95);
-            this.groupCall.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupCall.Location = new System.Drawing.Point(3, 3);
             this.groupCall.Name = "groupCall";
-            this.groupCall.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.groupCall.Size = new System.Drawing.Size(384, 221);
+            this.groupCall.Size = new System.Drawing.Size(373, 234);
             this.groupCall.TabIndex = 0;
             this.groupCall.TabStop = false;
             this.groupCall.Text = "Call";
@@ -75,9 +78,10 @@
             this.btnOpen.AutoSize = true;
             this.btnOpen.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.btnOpen.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnOpen.Location = new System.Drawing.Point(307, 26);
+            this.btnOpen.Location = new System.Drawing.Point(273, 22);
+            this.btnOpen.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnOpen.Name = "btnOpen";
-            this.btnOpen.Size = new System.Drawing.Size(35, 32);
+            this.btnOpen.Size = new System.Drawing.Size(33, 28);
             this.btnOpen.TabIndex = 6;
             this.btnOpen.Text = "...";
             this.btnOpen.UseVisualStyleBackColor = true;
@@ -85,62 +89,58 @@
             // 
             // label3
             // 
-            this.label3.Location = new System.Drawing.Point(9, 131);
+            this.label3.Location = new System.Drawing.Point(8, 109);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(118, 29);
+            this.label3.Size = new System.Drawing.Size(105, 24);
             this.label3.TabIndex = 5;
             this.label3.Text = "Working Dir";
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // textDir
             // 
-            this.textDir.Location = new System.Drawing.Point(134, 131);
-            this.textDir.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textDir.Location = new System.Drawing.Point(119, 109);
             this.textDir.Name = "textDir";
-            this.textDir.Size = new System.Drawing.Size(167, 28);
+            this.textDir.Size = new System.Drawing.Size(149, 25);
             this.textDir.TabIndex = 4;
             // 
             // label2
             // 
-            this.label2.Location = new System.Drawing.Point(9, 78);
+            this.label2.Location = new System.Drawing.Point(8, 65);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(118, 29);
+            this.label2.Size = new System.Drawing.Size(105, 24);
             this.label2.TabIndex = 3;
             this.label2.Text = "Parameter";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // textPara
             // 
-            this.textPara.Location = new System.Drawing.Point(134, 78);
-            this.textPara.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textPara.Location = new System.Drawing.Point(119, 65);
             this.textPara.Name = "textPara";
-            this.textPara.Size = new System.Drawing.Size(167, 28);
+            this.textPara.Size = new System.Drawing.Size(149, 25);
             this.textPara.TabIndex = 2;
             // 
             // label1
             // 
-            this.label1.Location = new System.Drawing.Point(9, 29);
+            this.label1.Location = new System.Drawing.Point(8, 24);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(118, 29);
+            this.label1.Size = new System.Drawing.Size(105, 24);
             this.label1.TabIndex = 1;
             this.label1.Text = "Run";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // textRun
             // 
-            this.textRun.Location = new System.Drawing.Point(134, 29);
-            this.textRun.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textRun.Location = new System.Drawing.Point(119, 24);
             this.textRun.Name = "textRun";
-            this.textRun.Size = new System.Drawing.Size(167, 28);
+            this.textRun.Size = new System.Drawing.Size(149, 25);
             this.textRun.TabIndex = 0;
             // 
             // button1
             // 
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.Location = new System.Drawing.Point(728, 473);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Location = new System.Drawing.Point(655, 357);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(160, 54);
+            this.button1.Size = new System.Drawing.Size(142, 45);
             this.button1.TabIndex = 1;
             this.button1.Text = "Generate Exe";
             this.button1.UseVisualStyleBackColor = true;
@@ -148,58 +148,65 @@
             // 
             // groupEmbed
             // 
+            this.groupEmbed.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.groupEmbed.Controls.Add(this.textEmbed);
             this.groupEmbed.Enabled = false;
-            this.groupEmbed.Location = new System.Drawing.Point(453, 95);
-            this.groupEmbed.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupEmbed.Location = new System.Drawing.Point(382, 3);
             this.groupEmbed.Name = "groupEmbed";
-            this.groupEmbed.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.groupEmbed.Size = new System.Drawing.Size(349, 221);
+            this.groupEmbed.Size = new System.Drawing.Size(374, 234);
             this.groupEmbed.TabIndex = 6;
             this.groupEmbed.TabStop = false;
             this.groupEmbed.Text = "Embed";
             // 
             // textEmbed
             // 
-            this.textEmbed.Location = new System.Drawing.Point(20, 29);
-            this.textEmbed.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textEmbed.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textEmbed.Location = new System.Drawing.Point(18, 24);
             this.textEmbed.Multiline = true;
             this.textEmbed.Name = "textEmbed";
-            this.textEmbed.Size = new System.Drawing.Size(307, 172);
+            this.textEmbed.Size = new System.Drawing.Size(340, 194);
             this.textEmbed.TabIndex = 6;
             // 
             // label4
             // 
-            this.label4.Location = new System.Drawing.Point(72, 361);
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label4.Location = new System.Drawing.Point(49, 327);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(118, 29);
+            this.label4.Size = new System.Drawing.Size(105, 24);
             this.label4.TabIndex = 7;
             this.label4.Text = "Icon File";
             this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // textIcon
             // 
-            this.textIcon.Location = new System.Drawing.Point(197, 361);
-            this.textIcon.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textIcon.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.textIcon.Location = new System.Drawing.Point(160, 327);
             this.textIcon.Name = "textIcon";
-            this.textIcon.Size = new System.Drawing.Size(167, 28);
+            this.textIcon.Size = new System.Drawing.Size(149, 25);
             this.textIcon.TabIndex = 6;
             // 
             // label5
             // 
-            this.label5.Location = new System.Drawing.Point(72, 403);
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label5.Location = new System.Drawing.Point(49, 362);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(118, 29);
+            this.label5.Size = new System.Drawing.Size(105, 24);
             this.label5.TabIndex = 9;
             this.label5.Text = "Show Console";
             this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // cbShowConsole
             // 
+            this.cbShowConsole.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.cbShowConsole.AutoSize = true;
-            this.cbShowConsole.Location = new System.Drawing.Point(197, 407);
+            this.cbShowConsole.Location = new System.Drawing.Point(160, 369);
+            this.cbShowConsole.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.cbShowConsole.Name = "cbShowConsole";
-            this.cbShowConsole.Size = new System.Drawing.Size(22, 21);
+            this.cbShowConsole.Size = new System.Drawing.Size(15, 14);
             this.cbShowConsole.TabIndex = 10;
             this.cbShowConsole.UseVisualStyleBackColor = true;
             // 
@@ -207,9 +214,10 @@
             // 
             this.radioCall.AutoSize = true;
             this.radioCall.Checked = true;
-            this.radioCall.Location = new System.Drawing.Point(80, 41);
+            this.radioCall.Location = new System.Drawing.Point(38, 34);
+            this.radioCall.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.radioCall.Name = "radioCall";
-            this.radioCall.Size = new System.Drawing.Size(150, 22);
+            this.radioCall.Size = new System.Drawing.Size(129, 19);
             this.radioCall.TabIndex = 11;
             this.radioCall.TabStop = true;
             this.radioCall.Text = "Call External";
@@ -218,9 +226,10 @@
             // radioEmbed
             // 
             this.radioEmbed.AutoSize = true;
-            this.radioEmbed.Location = new System.Drawing.Point(280, 41);
+            this.radioEmbed.Location = new System.Drawing.Point(216, 34);
+            this.radioEmbed.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.radioEmbed.Name = "radioEmbed";
-            this.radioEmbed.Size = new System.Drawing.Size(132, 22);
+            this.radioEmbed.Size = new System.Drawing.Size(113, 19);
             this.radioEmbed.TabIndex = 12;
             this.radioEmbed.Text = "Embed batch";
             this.radioEmbed.UseVisualStyleBackColor = true;
@@ -228,40 +237,58 @@
             // 
             // btnSelectIcon
             // 
+            this.btnSelectIcon.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.btnSelectIcon.AutoSize = true;
             this.btnSelectIcon.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.btnSelectIcon.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSelectIcon.Location = new System.Drawing.Point(370, 358);
+            this.btnSelectIcon.Location = new System.Drawing.Point(314, 325);
+            this.btnSelectIcon.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnSelectIcon.Name = "btnSelectIcon";
-            this.btnSelectIcon.Size = new System.Drawing.Size(35, 32);
+            this.btnSelectIcon.Size = new System.Drawing.Size(33, 28);
             this.btnSelectIcon.TabIndex = 7;
             this.btnSelectIcon.Text = "...";
             this.btnSelectIcon.UseVisualStyleBackColor = true;
             this.btnSelectIcon.Click += new System.EventHandler(this.btnSelectIcon_Click);
             // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.groupCall, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.groupEmbed, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(38, 58);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(759, 240);
+            this.tableLayoutPanel1.TabIndex = 13;
+            // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(900, 540);
+            this.ClientSize = new System.Drawing.Size(827, 414);
             this.Controls.Add(this.btnSelectIcon);
             this.Controls.Add(this.radioEmbed);
             this.Controls.Add(this.radioCall);
             this.Controls.Add(this.cbShowConsole);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.groupEmbed);
             this.Controls.Add(this.textIcon);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.groupCall);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "MainForm";
             this.Text = "ExWrapper 1.0";
             this.groupCall.ResumeLayout(false);
             this.groupCall.PerformLayout();
             this.groupEmbed.ResumeLayout(false);
             this.groupEmbed.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -294,5 +321,6 @@
         private System.Windows.Forms.RadioButton radioEmbed;
         private System.Windows.Forms.Button btnOpen;
         private System.Windows.Forms.Button btnSelectIcon;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
     }
 }
